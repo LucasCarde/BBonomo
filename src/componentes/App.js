757e344365a25e4cd385be33a124077a14ff6b7d
Carde <70904902+LucasCarde@users.noreply.github.com>
2023-11-24 19:28:55 -0300

    dejandolo un poquito mas prolijo pa la proxima nomas

--- src/componentes/App.js
+++ src/componentes/App.js
@@ -4,7 +4,7 @@ import Proyectos from "./Proyectos";
 
 function App() {
     const [pag, setPagina] = useState("Inicio")
-    function cambiarPagina(event){
+    function cambiarPagina(event) {
         const pagina = event.target.id;
         setPagina(pagina);
     }
@@ -17,9 +17,9 @@ function App() {
                 link1="Inicio"
                 link2="Proyectos"
                 link3="Fotografia"
-                cambiarPag = {cambiarPagina}
-                />
-            {pag == "Proyectos" && <Proyectos/>}
+                cambiarPag={cambiarPagina}/> {pag == "Proyectos" && <Proyectos/>}
+            {pag == "Inicio" && "inicio"}
+            {pag == "Fotografia" && "Fotografia"}
         </div>
     )
 }
